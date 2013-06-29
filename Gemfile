e5017da3c8adb5e9449f3f9bd846c2c6385ae4d2
Andreas Böhrnsen <andreas@boehrnsen.de>
2013-06-29 23:39:32 +0800

    fix foundation js loading

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem "middleman"
 gem "middleman-blog"
 
-gem "zurb-foundation"
+gem "zurb-foundation", require: false
 
 gem "middleman-livereload"
 gem "middleman-deploy"
--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,4 +1,8 @@
-//= require jquery
+// This is a manifest file that'll be compiled into all.js, which will include all the files
+// listed below.
+//
+//= require vendor/jquery
+//= require vendor/custom.modernizr
 //
 //= require foundation/foundation
 //= require foundation/foundation.alerts
