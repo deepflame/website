c09061aa91c2081cb13a9d7e98099cba628cc0e9
Andreas Böhrnsen <andreas@frontrunner.io>
2015-11-10 20:01:42 +0100

    cleanup config

--- config.rb
+++ config.rb
@@ -1,24 +1,29 @@
-###
-# Blog settings
-###
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
 
-# Time.zone = "UTC"
+Time.zone = "UTC" # used for Blog articles
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+
+activate :autoprefixer do |config|
+   config.browsers = ['last 2 versions', 'Explorer >= 9']
+end
 
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
   blog.prefix = "blog"
 
   blog.permalink = "{year}/{month}/{title}.html"
-  #blog.permalink = "{year}/{month}/{day}/{title}.html"
   blog.sources = "articles/{year}-{month}-{day}-{title}.html" # Matcher for blog source files
   blog.layout = "blog"
   blog.summary_separator = /(READMORE)/
   blog.summary_length = 250
-  # blog.taglink = "tags/{tag}.html"
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".md"
 
   blog.tag_template = "blog/tag.html"
   blog.calendar_template = "blog/calendar.html"
@@ -28,65 +33,27 @@ activate :blog do |blog|
   blog.per_page = 10
   blog.page_link = "page/{num}"
 end
-
 page "/blog/feed.xml", layout: false
-
-activate :syntax
-set :markdown_engine, :redcarpet
-set :markdown, 
-#              :no_intra_emphasis => true,
-#              :tables => true,
-               :fenced_code_blocks => true,
-#              :autolink => true,
-#              :strikethrough => true,
-#              :lax_html_blocks => true,
-#              :space_after_headers => true,
-#              :with_toc_data => true,
-#              :superscript => true,
-               :smartypants => true
-#              :hard_wrap => true
-
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
 
 activate :directory_indexes
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
 # load dotenv file
 activate :dotenv
+
+activate :syntax
+set :markdown_engine, :redcarpet
+set :markdown,
+#     :no_intra_emphasis => true,
+#     :tables => true,
+      :fenced_code_blocks => true,
+#     :autolink => true,
+#     :strikethrough => true,
+#     :lax_html_blocks => true,
+#     :space_after_headers => true,
+#     :with_toc_data => true,
+#     :superscript => true,
+      :smartypants => true
+#     :hard_wrap => true
 
 # deploy to AWS
 activate :s3_sync do |s3|
@@ -96,6 +63,7 @@ activate :s3_sync do |s3|
   s3.aws_secret_access_key = ENV['AWS_SECRET']
   s3.delete                = false
   s3.prefer_gzip           = true
+  #s3.after_build           = true
 end
 
 activate :cloudfront do |cf|
@@ -103,43 +71,20 @@ activate :cloudfront do |cf|
   cf.secret_access_key = ENV['AWS_SECRET']
   cf.distribution_id   = ENV['AWS_DISTID']
   cf.filter            = /\.html$/i  # default is /.*/
+  #cf.after_build       = true
 end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'assets/stylesheets'
-
-set :js_dir, 'assets/javascripts'
-
-set :images_dir, 'assets/images'
 
 # Development-specific configuration
 configure :development do
-  # Reload the browser automatically whenever files change
   activate :livereload
 end
 
 # Build-specific configuration
 configure :build do
-  # change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
-
-  # Enable cache buster
   activate :asset_hash
-
-  # Use relative URLs
   activate :relative_assets
-
-  # use a different image path
-  # set :http_path, "/Content/images/"
-
   ignore '**/LICENSE'
 end
+
